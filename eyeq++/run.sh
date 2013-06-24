d1b3c98e2cb2191a3e14a32417aecd85ac36548a
Vimalkumar <j.vimal@gmail.com>
2013-06-24 11:22:17 -0700

    Useful toggle to try out old htb

--- eyeq++/run.sh
+++ eyeq++/run.sh
@@ -5,12 +5,18 @@ dev=eth2
 tc qdisc del dev $dev root
 
 rmmod sch_htb sch_eyeq sch_prl
-insmod ./sch_eyeq.ko
+
+function new {
+    insmod ./sch_eyeq.ko
+}
+
+new
 
 tc qdisc add dev $dev root handle 1: htb default 1
 tc class add dev $dev classid 1:1 parent 1: htb rate 1Gbit
 tc class add dev $dev classid 1:10 parent 1:1 htb rate 1Gbit
 tc class add dev $dev classid 1:11 parent 1:1 htb rate 1Gbit
+tc class add dev $dev classid 1:12 parent 1:1 htb rate 1Gbit
 
 tc filter add dev $dev protocol ip parent 1: prio 1 u32 match \
     ip dport 5001 0xffff flowid 1:10
@@ -18,3 +24,5 @@ tc filter add dev $dev protocol ip parent 1: prio 1 u32 match \
 tc filter add dev $dev protocol ip parent 1: prio 1 u32 match \
     ip dport 5002 0xffff flowid 1:11
 
+tc filter add dev $dev protocol ip parent 1: prio 1 u32 match \
+    ip dport 5003 0xffff flowid 1:12
